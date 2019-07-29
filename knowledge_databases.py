630a93dbdafaf6d740b454c7746d3a37d46d2750
myname21-meet <myname21@meet.mit.edu>
2019-07-29 12:18:04 +0300

    database

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,20 +8,49 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(Title,Topic,Rate):
+	global art
+	art=Knowledge(Title=Title,Topic=Topic,Rate=Rate)
+	session.add(art)
+	session.commit()
 
-def query_all_articles():
-	pass
+
+def query_all_articles():#prints all the articals in the databaseb
+	arti=session.query(Knowledge).all()
+	return(arti)
 
 def query_article_by_topic():
-	pass
+	s1=session.query(Knowledge).filter_by(Rate=10).first()
+	return(s1)
+#def query_article_by_primary_key(key):
+	#s1=session.query(Knowledge).filter_by(id=1).first()
+	#return(s1)
 
-def delete_article_by_topic():
-	pass
+
+def delete_article_by_topic(r):
+	s1=session.query(Knowledge).filter_by(Rate=r).delete()
+	session.commit()
 
 def delete_all_articles():
-	pass
+	s1=session.query(Knowledge).delete()
+	session.commit()
 
-def edit_article_rating():
-	pass
+def edit_article_rating(ti,rate):
+	art1=session.query(Knowledge).filter_by(Title=ti).first()
+	art1.Rate=rate
+	session.commit()
+def delet_by_rate(r):
+	for i in range(-1,r+1):
+		delete_article_by_topic(i)
+	session.commit()
+
+
+
+
+add_article("try","viki",9)
+#print(query_all_articles())
+#print(query_article_by_topic())
+#delete_all_articles()
+#edit_article_rating("try",9)
+delet_by_rate(10)
+print(query_all_articles())
--- knowledge_model.py
+++ knowledge_model.py
@@ -6,6 +6,12 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
+	__tablename__="Articals"
+	articals_id=Column(Integer,primary_key=True)
+	Title=Column(String)
+	Topic=Column(String)
+	Rate=Column(Integer)
+	# print(articals_id)
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -13,5 +19,20 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	def prid(self):
+		print(self.articals_id)
 
-	pass
+	def __repr__(self):
+		if self.Rate>7:
+			return ("Title: {}\n"
+               "Topic: {} \n"
+               "Rate {}").format(
+                    self.Title, self.Topic, self.Rate)
+		else:
+			return("Unfortunately, this article does not have a better rating. Maybe, this is an article that should bereplaced soon!")
+
+#x=Knowledge(articals_id=1,Title="123")
+#x.prid()
+#print(x)
+#print(repr(Knowledge.__table__))
+
